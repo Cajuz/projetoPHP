d7c8ad5e09fb1f8dd28fe6256713cb3194a2e5a9
Alvaro Sáteles <138321157+Cajuz@users.noreply.github.com>
2024-08-12 20:03:41 -0300

    Add files via upload

---
+++ quarto.php
@@ -0,0 +1,14 @@
+<?php
+
+$data = "12 de Agosoto de 2024";
+$salario = 860.00;
+$idade = 18;
+$cargo = 'estagiario';
+$resultado = true;
+
+echo "data: $data <br />";
+echo "Salário: $salario <br />";
+echo "idade: $idade <br />";
+echo "cargo: $cargo <br />";
+echo "resultado: Verdadeiro";
+
