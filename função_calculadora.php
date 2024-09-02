e9e2adaa3e9e5f9662ebfb1e674d976db0cf04f9
Alvaro Sáteles <138321157+Cajuz@users.noreply.github.com>
2024-09-02 20:18:54 -0300

    Add files via upload

--- função_calculadora.php
+++ função_calculadora.php
@@ -1,28 +1,43 @@
 <?php
-$resp=3, $n1=3, $n2=5, $resultado=0;
+$resp=3; 
+$n1=3;
+$n2=5;
+$resultado=0;
 
-
-
-function calculadora ($n1, $n2){
 
 echo("Escolha o número referente a operação que vc quer efetuar com os dois números: ", $n1 "e ", $n2);
-echo("1- Soma               *3* Multiplicação \n
-      2- Subtração          4- Divisão");
+echo("1- Soma               *3* Multiplicação <- \n
+      2- Subtração           4- Divisão");
 
-      if ($resp=1) {
-        $resultado= $n1 + $n2;
-    } elseif ($resp=2 ) {
-        $resultado= $n1 - $n2;
+function soma ($n1, $n2){
+    return ($resultado= $n1 + $n2);
+}
+function subtraçao ($n1, $n2){
+    return ($resultado= $n1 - $n2);
+}
+function multiplicacao ($n1, $n2){
+    return ($resultado= $n1 * $n2);
+}
+function divisao ($n1, $n2){
+    return ($resultado= $n1 / $n2);
+}
+
+
+      if ($resp=1) {  
+         echo soma($n1, $n2);
+    } 
+    elseif ($resp=2 ) {
+        echo subtraçao($n1, $n2);
     } elseif ($resp=3 ) {
-        $resultado= $n1 * $n2;
+        echo multiplicacao($n1, $n2);
     }
     elseif ($resp=4 ) {
-        $resultado= $n1 / $n2;
+        echo divisao($n1, $n2);
     }
 
-return ($resultado);
 
-};
+
+
 
 
 
