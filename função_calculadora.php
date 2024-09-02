c49901b1e6f88c0d133496dbf5b84f9743c20e39
Alvaro Sáteles <138321157+Cajuz@users.noreply.github.com>
2024-09-02 20:24:25 -0300

    Add files via upload

--- função_calculadora.php
+++ função_calculadora.php
@@ -5,9 +5,9 @@ $n2=5;
 $resultado=0;
 
 
-echo("Escolha o número referente a operação que vc quer efetuar com os dois números: ", $n1 "e ", $n2);
-echo("1- Soma               *3* Multiplicação <- \n
-      2- Subtração           4- Divisão");
+echo("Escolha o número referente a operação que vc quer efetuar com os dois números: $n1  e  $n2 <br>");
+echo("1- Soma         <br>      *3* Multiplicação <- <br>
+      2- Subtração    <br>         4- Divisão <br>" );
 
 function soma ($n1, $n2){
     return ($resultado= $n1 + $n2);
@@ -24,14 +24,18 @@ function divisao ($n1, $n2){
 
 
       if ($resp=1) {  
+        echo("Resultado: ");
          echo soma($n1, $n2);
     } 
     elseif ($resp=2 ) {
+        echo("Resultado: ");
         echo subtraçao($n1, $n2);
     } elseif ($resp=3 ) {
+        echo("Resultado: ");
         echo multiplicacao($n1, $n2);
     }
     elseif ($resp=4 ) {
+        echo("Resultado: ");
         echo divisao($n1, $n2);
     }
 
