4e81ba0b26a963a5dc6dc40063d43deb7c4b7a3b
Alvaro Sáteles <138321157+Cajuz@users.noreply.github.com>
2024-09-30 20:23:55 -0300

    Add files via upload

---
+++ date_hours.php
@@ -0,0 +1,12 @@
+<?php
+date_default_timezone_set('UTC');
+
+$dataAtual = date("d/m/y");
+echo "<br/> Hoje é: " . $dataAtual."<br/>";
+
+$hora = time();
+echo "agora estamos a ".$hora." horas <br/>"
+
+
+
+?>
---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+$num1 = 0;
+$num2= 0;
+
+$resp = $num1 + $num2;
+
+echo "A soma de n1 e n2 é $resp";
+
+
+
+
+
+?>
